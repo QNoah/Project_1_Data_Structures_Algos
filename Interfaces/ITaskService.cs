28ccf3994251c75d869eb9eecc9e75bf446e85ea
Quinten <90308786+QNoah@users.noreply.github.com>
2026-03-02 20:08:05 +0100

    Uncommented ITaskService

--- Interfaces/ITaskService.cs
+++ Interfaces/ITaskService.cs
@@ -1,7 +1,7 @@
 public interface ITaskService
 {
     MyCollection<TaskItem> GetAllTasks();
-    // void AddTask(string description);
-    // void RemoveTask(int id);
-    // void ToggleTaskCompletion(int id);
+    void AddTask(string description);
+    void RemoveTask(int id);
+    void ToggleTaskCompletion(int id);
 }
