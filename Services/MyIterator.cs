9f87ad07bedc2006787fc20d5718b4ef430c8a0a
Quinten <90308786+QNoah@users.noreply.github.com>
2026-03-02 18:52:19 +0100

    Added service MyIterator

---
+++ Services/MyIterator.cs
@@ -0,0 +1,19 @@
+
+public class MyIterator<T> : IMyIterator<T>
+{
+    public bool HasNext()
+    {
+        //temporary
+        return false;
+    }
+    public T Next()
+    {
+        //temporary
+        return default;
+    }
+    public void Reset()
+    {
+        //temporary
+        return;
+    }
+}
