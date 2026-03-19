3b7eda4a715c4c4d54ddbd23d519452d0c2bd1de
Quinten <90308786+QNoah@users.noreply.github.com>
2026-03-19 13:42:16 +0100

    Changed model taskitem

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,9 +1,10 @@
 public class TaskItem
 {
-    public enum TaskPriority {P0, P1, P2}
-    public enum TaskStatus {ToDo, InProgress, Done}
+    public enum TaskPriority { P0, P1, P2 }
+    public enum TaskStatus { ToDo, InProgress, Done }
 
     public int Id { get; set; }
+    public int? ParentId { get; set; }
     public required string Title { get; set; }
     public required string Description { get; set; }
     public TaskPriority Priority { get; set; }
